3e5fc12a61ea9b2f3193ceb704c1249f91bc84d6
Emmanuel Oginga <ogolemmanuel3@gmail.com>
2025-12-10 10:49:06 +0300

    feat(form label):   hide label on input focus

--- script.js
+++ script.js
@@ -4,9 +4,32 @@ const close =document.getElementById("close")
 const scrollMenu = document.getElementById("menu-slide")
 
 menu.addEventListener("click", ()=>{
-    scrollMenu.classList.add("menu-slide-in")
+    scrollMenu.classList.add("menu-slide-in");
 });
 
 close.addEventListener("click", ()=>{
-    scrollMenu.classList.remove("menu-slide-in")
+    scrollMenu.classList.remove("menu-slide-in");
 });
+
+// form validation
+const form = document.getElementById("contact-form");
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const companyError = document.getElementById("companyError");
+const messageError = document.getElementById("messageError");
+const {fullname, email, companyName, message} = form.elements;
+
+// hide label on focus
+const inputs = [fullname, email, companyName, message];
+
+inputs.forEach((input) => {
+    const label = input.previousElementSibling;
+
+    input.addEventListener("focus",() => {
+        label.style.opacity = 0;
+    });
+
+    input.addEventListener("blur", () => {
+          label.style.opacity = 0.6;
+        });
+})
